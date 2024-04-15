80b6efa1c129e601042a1429a647b71693be61cc
Ebeca-EVMS <166470406+Ebeca-EVMS@users.noreply.github.com>
2024-04-15 23:17:08 +0800

    Add files via upload

--- assets/js/M01.js
+++ assets/js/M01.js
@@ -203,7 +203,7 @@ var currentLevelChart = new Chart(ctx, {
 //--car icon
 // Define car icon
 var carIcon = L.icon({
-    iconUrl: 'assets/images/car-icon.png', // Path to the car icon image
+    iconUrl: '/assets/images/car-icon.png', // Path to the car icon image
     iconSize: [32, 32], // Size of the icon
     iconAnchor: [16, 16], // Point of the icon which will correspond to marker's location
     popupAnchor: [0, -32] // Point from which the popup should open relative to the iconAnchor
